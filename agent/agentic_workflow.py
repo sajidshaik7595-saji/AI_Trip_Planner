424499aa87f0e07f5493e866b7e2adca30f756b8
sajidshaik <sajidshaik.7595@gmail.com>
2026-07-01 09:00:54 +0530

    updated

---
+++ agent/agentic_workflow.py
@@ -0,0 +1,22 @@
+from utils.model_loader import ModelLoader
+from prompt_library.prompt import SYSTEM_PROMPT
+from langgraph.graph import StateGraph,MessagesState,END,START
+from langgraph.prebuilt import ToolNode,tools_condition
+
+
+
+
+class GraphBuilder():
+    def __init__(self):
+        pass
+
+    def agent_function(self):
+        pass
+
+
+    def build_graph(self):
+        pass
+
+    def __call__(self):
+        pass
+
